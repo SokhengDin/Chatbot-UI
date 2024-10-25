142a02ebad0797705f2e24b7556f8ba9d48d276a
SokhengDin <din.sokheng08@gmail.com>
2024-10-25 16:57:29 +0700

    change to http response

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,8 @@
     <div class="min-h-screen flex flex-col bg-gray-100">
         <Header />
         <main class="flex-grow container mx-auto px-4 py-8">
-            <!-- <ChatInterface /> -->
-            <ChatStreamInterface />
+            <ChatInterface />
+            <!-- <ChatStreamInterface /> -->
         </main>
         <Footer />
     </div>
